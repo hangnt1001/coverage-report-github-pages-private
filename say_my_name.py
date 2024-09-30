26b5c376a1611786b66d08f90c10c5b8c15b7528
Harry Nguyen <gluesolution@gmail.com>
2024-09-30 11:42:01 +0700

    enable gh-pages

--- say_my_name.py
+++
@@ -1,9 +0,0 @@
-def say_my_name(name=None):
-    if name != "":
-        print(name + "??? You are right !!!")
-    else:
-        print("Heisenberg??? You are right !!!")
-
-
-if __name__ == "__main__":
-    say_my_name(input("Now.. Say My Name? "))
